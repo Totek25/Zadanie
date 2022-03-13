5f5a05f081513bc5774d374e979f1cd94129d37d
Tomek Toczynski <tomek-toczynski@wp.pl>
2022-03-13 13:03:52 +0100

    First commit

---
+++ src/main/java/Zadanie1.java
@@ -0,0 +1,9 @@
+class Zadanie1 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println("a+b = " + (a + b));
+        System.out.println("a-b = " + (a - b));
+    }
+}
